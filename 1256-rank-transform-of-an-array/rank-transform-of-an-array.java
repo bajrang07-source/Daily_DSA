3063691aca27084232468be55563e8fc7188e566
Bajrang Solunke <solunkebajrang679@gmail.com>
2026-07-13 00:55:45 +0530

    Time: 30 ms (71.06%) | Memory: 75 MB (81.34%) - LeetSync

---
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+
+        //Approach 1 - Encounter Time LIMIT EXCEEDED ISSUE
+
+        // int[] ans = new int[arr.length];
+        // int rank = 1;
+        // while (true) {
+        //     int min = Integer.MAX_VALUE;
+        //     for (int i = 0; i < arr.length; i++) {
+        //         if (arr[i] < min) {
+        //             min = arr[i];
+        //         }
+        //     }
+        //     if (min == Integer.MAX_VALUE) {
+        //         break;
+        //     }
+        //     for (int i = 0; i < arr.length; i++) {
+        //         if (arr[i] == min) {
+        //             ans[i] = rank;
+        //             arr[i] = Integer.MAX_VALUE;
+        //         }
+        //     }
+        //     rank++;
+        // }
+        // return ans;
+//----------------------------------------------------------------------------------
+        //APPROACH 2 - USING HASHMAP:
+        int[] arr1 = arr.clone();
+        Arrays.sort(arr1);
+        int rank = 1;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] ans = new int[arr1.length];
+        for(int i=0; i<arr1.length; i++){
+            if(map.containsKey(arr1[i])){
+                continue;
+            }else{
+                map.put(arr1[i], rank++);
+            }
+        }
+        for(int j=0; j<arr.length; j++){
+            int val = map.get(arr[j]);
+            ans[j] = val;
+        }
+        return ans;
+    }
+}
