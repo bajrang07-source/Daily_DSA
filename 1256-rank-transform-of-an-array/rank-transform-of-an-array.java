c432fbb2bd34457b455e511558babbb97d47e6ff
Bajrang Solunke <solunkebajrang679@gmail.com>
2026-07-13 00:58:18 +0530

    Reintroduce initial array ranking implementation
    Reintroduce the initial implementation for array ranking, addressing previous issues with time limits.

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -2,30 +2,30 @@ class Solution {
     public int[] arrayRankTransform(int[] arr) {
 
         //Approach 1 - Encounter Time LIMIT EXCEEDED ISSUE
-
-        // int[] ans = new int[arr.length];
-        // int rank = 1;
-        // while (true) {
-        //     int min = Integer.MAX_VALUE;
-        //     for (int i = 0; i < arr.length; i++) {
-        //         if (arr[i] < min) {
-        //             min = arr[i];
-        //         }
-        //     }
-        //     if (min == Integer.MAX_VALUE) {
-        //         break;
-        //     }
-        //     for (int i = 0; i < arr.length; i++) {
-        //         if (arr[i] == min) {
-        //             ans[i] = rank;
-        //             arr[i] = Integer.MAX_VALUE;
-        //         }
-        //     }
-        //     rank++;
-        // }
-        // return ans;
+        int[] ans = new int[arr.length];
+        int rank = 1;
+        while (true) {
+            int min = Integer.MAX_VALUE;
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] < min) {
+                    min = arr[i];
+                }
+            }
+            if (min == Integer.MAX_VALUE) {
+                break;
+            }
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] == min) {
+                    ans[i] = rank;
+                    arr[i] = Integer.MAX_VALUE;
+                }
+            }
+            rank++;
+        }
+        return ans;
 //----------------------------------------------------------------------------------
         //APPROACH 2 - USING HASHMAP:
+        
         int[] arr1 = arr.clone();
         Arrays.sort(arr1);
         int rank = 1;
