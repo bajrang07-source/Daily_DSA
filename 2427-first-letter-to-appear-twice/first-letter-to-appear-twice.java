430bf95755759adfa1c3606bde9b5576828fe1ff
Bajrang Solunke <solunkebajrang679@gmail.com>
2026-08-12 17:28:37 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (16.63%) - LeetSync

---
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.java
@@ -0,0 +1,12 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i=0; i<s.length(); i++){
+            if(map.containsKey(s.charAt(i))) return s.charAt(i);
+            else{
+                map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+            }
+        }
+        return '\0';
+    }
+}
