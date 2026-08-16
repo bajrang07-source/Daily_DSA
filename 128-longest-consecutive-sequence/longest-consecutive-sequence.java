5314f7f53840e7dccce015cdf177b76b8942d4c7
Bajrang Solunke <solunkebajrang679@gmail.com>
2026-08-16 15:18:21 +0530

    Time: 21 ms (95.69%) | Memory: 77.5 MB (87.37%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -3,41 +3,41 @@ class Solution {
 
         //---Initial Approach----
 
-        // if(nums.length<1) return 0;
-        // Arrays.sort(nums);
-        // int count = 1;
-        // int ans = 0;
-        // for(int i=1; i<nums.length; i++){
-        //     if(nums[i] - nums[i-1] == 1){
-        //         count++;
-        //     }
-        //     else if(nums[i] == nums[i-1]){
-        //         continue;
-        //     }else{
-        //         ans = Math.max(ans, count);
-        //         count=1;
-        //     }
-        // }
-        // return Math.max(ans, count);
-
-        //Approach considering TC:-
-        HashSet<Integer> set = new HashSet<>();
-        for(int num : nums){
-            set.add(num);
-        }
+        if(nums.length<1) return 0;
+        Arrays.sort(nums);
+        int count = 1;
         int ans = 0;
-        for(int val : set){
-            if(!set.contains(val-1)){
-                int count = 1;
-                int c_val = val;
-
-                while(set.contains(c_val+1)){
-                    count++;
-                    c_val++;
-                }
+        for(int i=1; i<nums.length; i++){
+            if(nums[i] - nums[i-1] == 1){
+                count++;
+            }
+            else if(nums[i] == nums[i-1]){
+                continue;
+            }else{
                 ans = Math.max(ans, count);
+                count=1;
             }
         }
-        return ans;
+        return Math.max(ans, count);
+
+        //Approach considering TC:-
+        // HashSet<Integer> set = new HashSet<>();
+        // for(int num : nums){
+        //     set.add(num);
+        // }
+        // int ans = 0;
+        // for(int val : set){
+        //     if(!set.contains(val-1)){
+        //         int count = 1;
+        //         int c_val = val;
+
+        //         while(set.contains(c_val+1)){
+        //             count++;
+        //             c_val++;
+        //         }
+        //         ans = Math.max(ans, count);
+        //     }
+        // }
+        // return ans;
     }
 }
