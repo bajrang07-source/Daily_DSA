edfee7461db2af248dfe134848242b1ff2101ea7
Bajrang Solunke <solunkebajrang679@gmail.com>
2026-08-16 15:15:45 +0530

    Time: 38 ms (13.27%) | Memory: 95.9 MB (8.69%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,20 +1,43 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        if(nums.length<1) return 0;
-        Arrays.sort(nums);
-        int count = 1;
+
+        //---Initial Approach----
+
+        // if(nums.length<1) return 0;
+        // Arrays.sort(nums);
+        // int count = 1;
+        // int ans = 0;
+        // for(int i=1; i<nums.length; i++){
+        //     if(nums[i] - nums[i-1] == 1){
+        //         count++;
+        //     }
+        //     else if(nums[i] == nums[i-1]){
+        //         continue;
+        //     }else{
+        //         ans = Math.max(ans, count);
+        //         count=1;
+        //     }
+        // }
+        // return Math.max(ans, count);
+
+        //Approach considering TC:-
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums){
+            set.add(num);
+        }
         int ans = 0;
-        for(int i=1; i<nums.length; i++){
-            if(nums[i] - nums[i-1] == 1){
-                count++;
-            }
-            else if(nums[i] == nums[i-1]){
-                continue;
-            }else{
+        for(int val : set){
+            if(!set.contains(val-1)){
+                int count = 1;
+                int c_val = val;
+
+                while(set.contains(c_val+1)){
+                    count++;
+                    c_val++;
+                }
                 ans = Math.max(ans, count);
-                count=1;
             }
         }
-        return Math.max(ans, count);
+        return ans;
     }
 }
