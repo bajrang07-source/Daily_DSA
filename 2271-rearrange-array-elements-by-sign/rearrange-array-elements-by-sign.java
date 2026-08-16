c2b26a890da3fe2252f80389a2c3106ef819858e
Bajrang Solunke <solunkebajrang679@gmail.com>
2026-08-16 13:26:45 +0530

    Time: 3 ms (100.00%) | Memory: 175.7 MB (28.60%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -2,20 +2,22 @@ class Solution {
     public int[] rearrangeArray(int[] nums) {
         int p_idx = 0;
         int n_idx = 1;
-        HashMap<Integer, Integer> map = new HashMap<>();
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        int[] ans = new int[nums.length];
         for(int i=0; i<nums.length; i++){
             if(nums[i] > 0){
-                map.put(p_idx, nums[i]);
+                // map.put(p_idx, nums[i]);
+                ans[p_idx] = nums[i];
                 p_idx += 2;
             }else{
-                map.put(n_idx, nums[i]);
+                // map.put(n_idx, nums[i]);
+                ans[n_idx] = nums[i];
                 n_idx += 2;
             }
         }
-        int[] ans = new int[nums.length];
-        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
-            ans[entry.getKey()] = entry.getValue();
-        }
+        // for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+        //     ans[entry.getKey()] = entry.getValue();
+        // }
         return ans;
     }
 }
