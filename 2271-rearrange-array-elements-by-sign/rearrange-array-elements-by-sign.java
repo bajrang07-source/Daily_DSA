286b1722ed0065ce5029a4b52e490d1378453be7
Bajrang Solunke <solunkebajrang679@gmail.com>
2026-08-16 13:24:14 +0530

    Time: 45 ms (6.63%) | Memory: 228 MB (5.22%) - LeetSync

---
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        int p_idx = 0;
+        int n_idx = 1;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] > 0){
+                map.put(p_idx, nums[i]);
+                p_idx += 2;
+            }else{
+                map.put(n_idx, nums[i]);
+                n_idx += 2;
+            }
+        }
+        int[] ans = new int[nums.length];
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            ans[entry.getKey()] = entry.getValue();
+        }
+        return ans;
+    }
+}
