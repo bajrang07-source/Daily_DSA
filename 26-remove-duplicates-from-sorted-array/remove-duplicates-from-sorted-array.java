927ca7f13d47499b421d80b1d9957ed760985849
Bajrang Solunke <solunkebajrang679@gmail.com>
2026-08-29 16:06:39 +0530

    Time: 1 ms (77.76%) | Memory: 46.4 MB (92.04%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,14 +1,29 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        Set<Integer> set = new TreeSet<>();
-        for(int i = 0; i < nums.length; i++) {
-            set.add(nums[i]);
-        }
-        int i=0;
-        for(int val : set) {
-            nums[i++] = val;
-        }
+        //---------BRUTE-FORCE APPROACH--------------
+        // Set<Integer> set = new TreeSet<>();
+        // for(int i = 0; i < nums.length; i++) {
+        //     set.add(nums[i]);
+        // }
+        // int i=0;
+        // for(int val : set) {
+        //     nums[i++] = val;
+        // }
 
-        return set.size();
+        // return set.size();
+
+        //------------OPTIMAL APPROACH-----------------
+        int i=0; 
+        int j=1;
+        while(j < nums.length) {
+            if(nums[i] == nums[j]) {
+                j++;
+            } else {
+                i++;
+                nums[i] = nums[j];
+                j++;
+            }
+        }
+        return i+1;
     }
 }
