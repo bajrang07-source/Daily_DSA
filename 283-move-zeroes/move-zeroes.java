520c09e53aeaef56c73a925c31791bd2b372045f
Bajrang Solunke <solunkebajrang679@gmail.com>
2026-09-07 13:21:47 +0530

    Time: 30 ms (6.05%) | Memory: 47.9 MB (38.17%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,22 +1,52 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int[] temp = new int[nums.length];
-        int noOfZeros = 0;
-        int idx = 0;
 
-        for(int i = 0; i < nums.length; i++) {
-            if(nums[i] == 0) noOfZeros++;
-            else{
-                temp[idx++] = nums[i];
+//-------------------      BRUTE FORCE APPROACH       -------------------------------
+        // int[] temp = new int[nums.length];
+        // int noOfZeros = 0;
+        // int idx = 0;
+
+        // for(int i = 0; i < nums.length; i++) {
+        //     if(nums[i] == 0) noOfZeros++;
+        //     else{
+        //         temp[idx++] = nums[i];
+        //     }
+        // }
+
+        // for(int j = 0; j < noOfZeros; j++) {
+        //     temp[idx++] = 0;
+        // }
+
+        // for(int k = 0; k < nums.length; k++) {
+        //     nums[k] = temp[k];
+        // }
+
+//-------------------      OPTIMAL APPROACH       -------------------------------
+
+        int i = -1;
+        int j = -1;
+        for(int idx = 0; idx < nums.length; idx++) {
+
+            if(nums[idx] == 0) {
+                j = idx;
+                i = idx + 1;
+                break;
             }
         }
 
-        for(int j = 0; j < noOfZeros; j++) {
-            temp[idx++] = 0;
-        }
+        if(j == -1) return;
 
-        for(int k = 0; k < nums.length; k++) {
-            nums[k] = temp[k];
+        while(i < nums.length) {
+            if(nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+                i = j + 1;
+            }
+            else{
+                i++;
+            }
         }
     }
 }
