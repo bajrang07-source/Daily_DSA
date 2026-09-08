1a083a948343db002414a59ee8ded584d1f1f045
Bajrang Solunke <solunkebajrang679@gmail.com>
2026-09-08 12:48:45 +0530

    Time: 1 ms (83.65%) | Memory: 42.6 MB (39.85%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int countCommas(int n) {
+        String num = Integer.toString(n);
+
+        if(num.length() < 4) return 0;
+
+        // int ans = 0;
+        return n-999;
+    }
+}
